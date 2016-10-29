319e225bf044b94c8ad2eb7a70381b1a68060cb8
Steven Brice <steven.brice@vividseats.com>
2016-10-28 23:53:43 -0500

    Turn App into pure function and add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => (
+  <div className="Header">
+    <div className="Header-container">
+      <div className="left">
+        Left
+      </div>
+      <div className="right">
+        Right
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,19 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Header from '../../components/Header';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = (props) => (
+  <div className="App">
+    <Header />
+    <div className="App-body">
+      {props.children}
+    </div>
+  </div>
+);
 
 export default App;
