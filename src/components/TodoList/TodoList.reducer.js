c3c425ed17d471bd1f06db67714a178f05a14c3e
Steven Brice <steven.brice@vividseats.com>
2016-10-29 01:10:41 -0500

    Wireup reducer to add new todo

--- src/components/TodoList/TodoList.reducer.js
+++ src/components/TodoList/TodoList.reducer.js
@@ -2,30 +2,41 @@ import { ADD_TODO, DELETE_TODO, COMPLETE_TODO, COMPLETE_ALL } from './TodoList.a
 import { combineReducers } from 'redux';
 
 const initialState = [
-    {
-        id: 0,
-        text: 'New Entry',
-        completed: true
-    }
+  {
+    id: 0,
+    text: 'New Entry',
+    completed: true
+  }
 ];
 
+let currentId = 0;
+
 function todos(state = initialState, action) {
-    switch (action.type) {
-        case ADD_TODO:
-            break;
-        case DELETE_TODO:
-            break;
-        case COMPLETE_TODO:
-            break;
-        case COMPLETE_ALL:
-            break;
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        {
+          id: currentId++,
+          text: action.text,
+          completed: false
+        },
+        ...state
+      ];
+
+    case DELETE_TODO:
+      break;
+    case COMPLETE_TODO:
+      break;
+    case COMPLETE_ALL:
+      break;
+    default:
+  }
+
+  return state;
+};
 
 const TodoList = combineReducers({
-    todos
-})
+  todos
+});
 
 export default TodoList;
