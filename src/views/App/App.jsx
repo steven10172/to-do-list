687f811ac5e94dd9158dfdb40265a26ea82bd76a
Steven Brice <steven.brice@vividseats.com>
2016-10-29 01:54:59 -0500

    Wireup App to store and pass props/actions

--- src/views/App/App.jsx
+++ src/views/App/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Header from '../../components/Header';
+import Dashboard from '../Dashboard';
+import * as TodoActions from '../../components/TodoList/TodoList.actions.js'
 import './App.css';
 
-const App = (props) => (
+const App = ({todoApp, actions}) => (
   <div className="App">
     <Header />
     <div className="App-body">
-      {props.children}
+      <Dashboard addTodo={actions.addTodo} todoApp={todoApp} />
     </div>
   </div>
 );
 
-export default App;
+App.propTypes = {
+  todoApp: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  todoApp: state.todoApp
+})
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(TodoActions, dispatch)
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
